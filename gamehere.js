bac24ef34eb9108343a5f9da2dace7dc566a210b
Kittybunny231 <163947291+Kittybunny231@users.noreply.github.com>
2024-08-06 14:08:37 -0400

    addedfunction getComputerChoice and math

--- gamehere.js
+++ gamehere.js
@@ -1,1 +1,12 @@
-console.log("Hello World") 
+
+function getComputerChoice(min, max) {
+     return Math.round(Math.random() * (10 - 1) + 1);
+}
+
+let compChoice = getComputerChoice();
+
+if (compChoice % 3 === 0) {
+    console.log("Paper");
+    }  else if (compChoice % 4 === 0) {
+        console.log("Scissors");
+  } else  console.log(compChoice = "Rock");
